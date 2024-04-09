93926e1a80cf670643e6fa897110b3bf252f136e
Hugo Esteban Quinteros Bustos <quinteros.hugo@externos.bci.cl>
2024-04-09 18:59:39 -0400

    fix h1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,10 +8,7 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : 'Loading'}
-                  <span></span>
-                </h1>
+                <h1>{props.data ? props.data.title : 'Loading'}</h1>
                 <p>{props.data ? props.data.paragraph : 'Loading'}</p>
                 <a
                   href="#features"
