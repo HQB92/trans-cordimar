7a59ae325d6d60d484eec77d1338e259dba44249
Hugo Esteban Quinteros Bustos <quinteros.hugo@externos.bci.cl>
2024-04-09 17:26:35 -0400

    primer commit

---
+++ src/components/navigation.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {' '}
+            <span className="sr-only">Toggle navigation</span>{' '}
+            <span className="icon-bar"></span>{' '}
+            <span className="icon-bar"></span>{' '}
+            <span className="icon-bar"></span>{' '}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Trans-Cordimar
+          </a>{' '}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                Nosotros
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Servicios
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Galeria
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonios
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="page-scroll">
+                Equipo
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Compromiso</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {' '}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  );
+};
